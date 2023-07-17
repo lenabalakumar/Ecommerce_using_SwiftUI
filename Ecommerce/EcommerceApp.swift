8ef661d58b02be2943445124ab204129146aa322
Lenabalakumar Subbarayan <lenabalakumar@gmail.com>
2023-07-17 11:15:56 +0100

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Lenabalakumar Subbarayan on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
